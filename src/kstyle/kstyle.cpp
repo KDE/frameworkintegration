abcaf01e7431b4a5ef2b60ff4f77034c82e4b333
David Faure <faure@kde.org>
2013-12-31 00:41:50 +0100

    Revert d2dad4199d, "Reparse the configuration each time styleHint is called".
    This is just crazy for performance reasons, even as a "workaround".
    Let's use a DBus signal in kstyle instead. The early revert is for TP1 to be usable.
    CCMAIL: afiestas@kde.org

--- src/kstyle/kstyle.cpp
+++ src/kstyle/kstyle.cpp
@@ -335,7 +335,6 @@ QIcon KStyle::standardIcon(StandardPixmap standardIcon, const QStyleOption */*op
 
 int KStyle::styleHint(StyleHint hint, const QStyleOption *option, const QWidget *widget, QStyleHintReturn *returnData) const
 {
-    KSharedConfig::openConfig()->reparseConfiguration();
     switch (hint) {
     case SH_ItemView_ActivateItemOnSingleClick: {
         KConfigGroup g(KSharedConfig::openConfig(), "KDE");
