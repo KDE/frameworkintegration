2362789ff589daf533b3e795653bb8a47710aacb
Ahmad Samir <a.samirh78@gmail.com>
2020-12-04 20:39:12 +0200

    Pass parent to Q*Layout ctor instead of calling setLayout()

--- tests/kstyletest.cpp
+++ tests/kstyletest.cpp
@@ -18,7 +18,8 @@
 void showDialog()
 {
     QScopedPointer<QDialog> dialog(new QDialog);
-    dialog->setLayout(new QVBoxLayout);
+
+    auto *layout = new QVBoxLayout(dialog.data());
 
     QDialogButtonBox *box = new QDialogButtonBox(QDialogButtonBox::Cancel | QDialogButtonBox::Ok, dialog.data());
 
@@ -28,8 +29,8 @@ void showDialog()
     QObject::connect(box, &QDialogButtonBox::rejected, dialog.data(), &QDialog::reject);
 
     auto usefulWidget = new QTextEdit(dialog.data());
-    dialog->layout()->addWidget(usefulWidget);
-    dialog->layout()->addWidget(box);
+    layout->addWidget(usefulWidget);
+    layout->addWidget(box);
 
     //Make sure we test ctrl+return acceptance with the focus on the button
     usefulWidget->setFocus();
@@ -44,11 +45,11 @@ int main(int argc, char **argv)
     app.setStyle(new KStyle);
 
     QWidget w;
-    w.setLayout(new QVBoxLayout);
+    auto *layout = new QVBoxLayout(&w);
 
     QPushButton *showDialogButton = new QPushButton(QStringLiteral("Dialog"), &w);
     QObject::connect(showDialogButton, &QPushButton::clicked, &showDialog);
-    w.layout()->addWidget(showDialogButton);
+    layout->addWidget(showDialogButton);
     w.resize(200, 200);
     w.show();
 
